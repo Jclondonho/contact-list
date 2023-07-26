c5e571a93159f48ad91642a54b0be137702c7d98
Juan <juanclb1031@gmail.com>
2023-07-25 22:40:23 -0500

    se agrego la funcion de borrar e imprimir contacto

--- datos.js
+++ datos.js
@@ -18,8 +18,7 @@
 
         }
 
-    
-    // borrarContacto(arrayLista);
+
     // imprimir (arrayLista);
 
     function agregarLista (id,nombre,apellido,telefono,ciudad,direccion){
@@ -40,22 +39,29 @@
     agregarLista(1, "Henry","perez",3003208877,"cartagena","calle5" );
     agregarLista(2, "daniel","guerrero",314888913,"sincelejo","calle6");
     agregarLista(3, "carlos","mejia",3127653421,"monteria","calle7" );
+    
     console.log("lista nueva");
     for (let i = 0; i < arrayDeObjetos.length; i ++){
         console.log(arrayDeObjetos[i]);
 
     }
-    // function borrarContacto(contactoEliminado){
-    //     console.log("contacto eliminado: " + contactoEliminado.pop());
-    // }
 
-    // function imprimir (arrayLista1){
-    //     console.log("nueva lista es: ");
-    //     for (let i = 0; i < arrayLista1.length; i ++){
-    //         console.log(arrayLista1[i]);
-    //     }
+    borrarContacto(arrayDeObjetos);
+
+    imprimir (arrayDeObjetos);
+
+
+    function borrarContacto(contactoEliminado){
+        console.log("contacto eliminado: " + contactoEliminado.pop());
+    }
+
+    function imprimir (arrayLista1){
+        console.log("nueva lista con el contacto Eliminado: ");
+        for (let i = 0; i < arrayLista1.length; i ++){
+            console.log(arrayLista1[i]);
+        }
         
-    // }
+    }
 
     //Recorer Arrays con for
     //Para buscar algo en especifico While
